d6157bd6289ec84763acd9c0b439d1ee5d1ee095
Vaibhav Sinha <vaibhavleoanto@gmail.com>
2021-07-03 12:33:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var fs = require('fs');
+var path = require('path');
+var csv = require('csv-parser');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+let codes = [];
+var app = express();
+app.get("/home", (req,res)=>{
+  res.sendFile(path.join(__dirname,"/index.html")); 
+})
+app.get("/encrypt",(req,res)=>{
+   codes=[]
+   fs.createReadStream('data.csv').pipe(csv()).on('data', (row)=>{
+    codes.push(row.codes);
+  })  
+  res.send("Codes Retrieved")
+})
+
+app.post("/done",(req,res)=>{
+   console.log(req.body)
+   console.log(codes);
+   res.send("Done!!");
+})
+
+
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+app.listen(3000, ()=>{
+  console.log("Server started")
+});
+
+module.exports = app;
